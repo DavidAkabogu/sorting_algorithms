b857c1fbae6255e0f47cb2d490d5863e3e65ed25
David King <akabogudavid@gmail.com>
2023-06-27 12:49:13 -0700

    sorting algorithms and big o notation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,17 +1,17 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_integers - Swap two integers in an array.
+ * @i: The first integer to swap.
+ * @j: The second integer to swap.
  */
-void swap_ints(int *a, int *b)
+void swap_integers(int *i, int *j)
 {
-	int tmp;
+	int temporary;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+	temporary = *i;
+	*i = *j;
+	*j = temporary;
 }
 
 /**
@@ -19,28 +19,28 @@ void swap_ints(int *a, int *b)
  *              order using the shell sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
- *
  * Description: Uses the Knuth interval sequence.
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t interval, i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
-
-	for (; gap >= 1; gap /= 3)
+	for (interval = 1; interval < (size / 3);)
 	{
-		for (i = gap; i < size; i++)
+		interval = interval * 3 + 1;
+	}
+	for (; interval >= 1; interval /= 3)
+	{
+		for (i = interval; i < size; i++)
 		{
 			j = i;
-			while (j >= gap && array[j - gap] > array[j])
+			while (j >= interval && array[j - interval] > array[j])
 			{
-				swap_ints(array + j, array + (j - gap));
-				j -= gap;
+				swap_integers(array + j, array + (j - interval));
+				j -= interval;
 			}
 		}
 		print_array(array, size);
